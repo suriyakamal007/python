622cec0170e8eaab85cda2ab3f2554af5e0ec0be
suriyakamal007 <54100033+suriyakamal007@users.noreply.github.com>
2019-08-14 15:29:11 +0530

    Add files via upload

--- alpha.py
+++ alpha.py
@@ -1,6 +1,6 @@
 n=str(input())
 if n=='a' or n=='e' or n=='i' or n=='o' or n=='u' or n=='A' or n=='E' or n=='I' or n=='O' or n=='U':
-  print("Alphabet")
+  print("Vowel")
 elif n.isalpha():
   print("Consonant")
 else:

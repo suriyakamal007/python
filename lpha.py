11bd3462cb6f7f0c74400153adba6136d4a2517e
suriyakamal007 <54100033+suriyakamal007@users.noreply.github.com>
2019-08-14 15:37:30 +0530

    Rename alpha.py to lpha.py

---
+++ lpha.py
@@ -0,0 +1,7 @@
+n=str(input())
+if n=='a' or n=='e' or n=='i' or n=='o' or n=='u' or n=='A' or n=='E' or n=='I' or n=='O' or n=='U':
+  print("Vowel")
+elif n.isalpha():
+  print("Consonant")
+else:
+    print("invalid")
